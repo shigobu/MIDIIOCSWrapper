36254760e1721893fd46f905dde8199373538193
shigobu <shigenobu314@yahoo.co.jp>
2018-11-27 21:43:49 +0900

    テストアプリを変更。 メセージを表示するようにした

--- テストアプリ/Program.cs
+++ テストアプリ/Program.cs
@@ -11,13 +11,64 @@ namespace テストアプリ
     {
         static void Main(string[] args)
         {
-            int deviceNum = MIDIIN.GetDeviceNum();
-            Console.WriteLine(deviceNum);
-            for (int i = 0; i < deviceNum; i++)
+            // MIDI入力デバイス(No.0)の名前を調べる:
+
+            int InDev数 = MIDIIN.GetDeviceNum();
+
+            Console.WriteLine("MIDI入力デバイスの選択");
+            for (int i = 0; i < InDev数; i++)
             {
-                Console.WriteLine(MIDIIN.GetDeviceName(i));
+                Console.WriteLine(i + " " + MIDIIN.GetDeviceName(i));
             }
-            Console.Read();
+
+            Console.Write(">");
+            int InSelect;
+            bool 変換成功 = int.TryParse(Console.ReadLine(), out InSelect);
+
+            while (InSelect >= InDev数 || !変換成功)
+            {
+                Console.WriteLine("正しい数値を入力してください！！");
+                Console.Write(">");
+                System.Media.SystemSounds.Beep.Play();
+                変換成功 = int.TryParse(Console.ReadLine(), out InSelect);
+            }
+
+            string InDeviceName = MIDIIN.GetDeviceName(InSelect);
+
+            // MIDI入力デバイスを開く:
+            MIDIIN midiIn = new MIDIIN(InDeviceName);
+            try
+            {
+                Console.WriteLine("MIDI入力デバイス「{0:s}」を開きました", InDeviceName);
+
+                Console.Beep();
+
+                System.Threading.Thread.Sleep(1000);
+
+                while (Console.KeyAvailable == false)
+                {
+                    // MIDI入力デバイスからMIDIメッセージを1つ取得する:
+                    byte[] byMessage = midiIn.GetMIDIMessage();
+                    // MIDIメッセージを取得した場合:
+                    if (byMessage.Length > 0)
+                    {
+                        for (int i = 0; i < byMessage.Length; ++i)
+                        {
+                            Console.Write("0x{0:x2} ", byMessage[i]);
+                        }
+                        Console.Write("\n");
+                    }
+                    else
+                    {
+                        System.Threading.Thread.Sleep(1);
+                    }
+                }
+
+            }
+            finally
+            {
+                midiIn.Close();
+            }
         }
     }
 }
