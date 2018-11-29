edba339b58f23a2fc53a6212dee4b5654181a194
sh-kasamatsu <sh-kasamatsu@apptec.co.jp>
2018-11-29 15:23:34 +0900

    IDisposableを実装

--- MIDIIOCSWrapper/MIDIIN.cs
+++ MIDIIOCSWrapper/MIDIIN.cs
@@ -21,7 +21,7 @@ namespace MIDIIOCSWrapper
         public byte m_byRunningStatus;
     }
 
-    public class MIDIIN
+    public class MIDIIN : IDisposable
     {
         #region DLLインポート
 
@@ -316,5 +316,43 @@ namespace MIDIIOCSWrapper
                 }
             }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // 重複する呼び出しを検出するには
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: マネージ状態を破棄します (マネージ オブジェクト)。
+                }
+
+                // TODO: アンマネージ リソース (アンマネージ オブジェクト) を解放し、下のファイナライザーをオーバーライドします。
+                // TODO: 大きなフィールドを null に設定します。
+                //MIDIデバイスを閉じる
+                this.Close();
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 上の Dispose(bool disposing) にアンマネージ リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします。
+        ~MIDIIN()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+            Dispose(false);
+        }
+
+        // このコードは、破棄可能なパターンを正しく実装できるように追加されました。
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+            Dispose(true);
+            // TODO: 上のファイナライザーがオーバーライドされる場合は、次の行のコメントを解除してください。
+            GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
